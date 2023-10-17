6174af61c1039bd16bc8641578599a532f1dc5eb
KenStarry <starrycodes@gmail.com>
2023-10-17 22:56:36 +0300

    Added Colours

---
+++ lib/theme/colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const white = Colors.white;
+const black = Colors.black;
+
+const primary = Color(0xff98B130);
+const primaryAlt = Color(0xff00681D);
+const secondary = Color(0xffF9B420);
+
+const textAccentDark = Color(0xff243B25);
+const textAccentLight = Color(0xff00681D);
+const textAccentLighter = Color(0xff243B25);
+const textBlack900 = Color(0xFF000000);
+const textBlack700 = Color(0xFF171717);
+const textBlack500 = Color(0xFF353535);
+const textBlack300 = Color(0xFF535353);
+const textBlack100 = Color(0xFF5F5F5F);
+
+const gradient = LinearGradient(colors: [
+  Color(0xffF9B420),
+  Color(0xffF7C02D),
+  Color(0xff98B130),
+]);
