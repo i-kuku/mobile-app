f725b3746049a1eb02f35f3e54bf85a4f4c875be
KenStarry <starrycodes@gmail.com>
2023-11-02 14:11:36 +0300

    Easy localizations translation

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:get/get.dart';
 import 'package:ikuku/features/feature_onboarding/presentation/screens/welcome_screen.dart';
 import 'package:ikuku/theme/my_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   runApp(EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('kisw')],
       path: 'assets/translations',
       fallbackLocale: const Locale('en', 'US'),
+      saveLocale: true,
       child: const MyApp()));
 }
 
@@ -19,6 +23,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: const WelcomeScreen(),
       theme: MyTheme.lightTheme,
       darkTheme: MyTheme.lightTheme,
