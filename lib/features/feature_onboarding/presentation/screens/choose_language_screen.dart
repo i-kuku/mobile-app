17b87daee4b27ab18a0786b7550482bf90333407
KenStarry <starrycodes@gmail.com>
2023-11-02 14:13:14 +0300

    Added Choose Language Screen

---
+++ lib/features/feature_onboarding/presentation/screens/choose_language_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseLanguageScreen extends StatefulWidget {
+  const ChooseLanguageScreen({super.key});
+
+  @override
+  State<ChooseLanguageScreen> createState() => _ChooseLanguageScreenState();
+}
+
+class _ChooseLanguageScreenState extends State<ChooseLanguageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
