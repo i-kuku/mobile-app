acdead02af872929e836480709cfa5da599c7de1
KenStarry <starrycodes@gmail.com>
2023-11-02 14:52:39 +0300

    Active language setup

--- lib/features/feature_onboarding/presentation/controller/onboarding_controller.dart
+++ lib/features/feature_onboarding/presentation/controller/onboarding_controller.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
 
 class OnboardingController extends GetxController {
+  final activeLanguage = 'English'.obs;
 
+  void setActiveLanguage({required String languageTitle}) =>
+      activeLanguage.value = languageTitle;
 }
