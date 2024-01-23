212d0c2ea39df155edda2da36c76f0d06203bb5a
KenStarry <starrycodes@gmail.com>
2024-01-24 00:01:24 +0300

    Verification Screen UI adjustments

--- lib/features/feature_sign_up/presentation/screens/verification_screen.dart
+++ lib/features/feature_sign_up/presentation/screens/verification_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class VerificationScreen extends StatefulWidget {
   const VerificationScreen({super.key});
@@ -11,9 +14,44 @@ class _VerificationScreenState extends State<VerificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () => Get.back(),
+            icon: Icon(Icons.arrow_back_rounded,
+                color: Theme.of(Get.context!).iconTheme.color)),
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Theme.of(context).scaffoldBackgroundColor,
+            statusBarIconBrightness: Brightness.dark),
+      ),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Column(
-        children: [],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //  image
+            SvgPicture.asset('assets/images/verify_phone.svg'),
+
+            const SizedBox(height: 24),
+
+            //  title
+            Text('Verify Phone Number',
+                style: Theme.of(context).textTheme.titleMedium),
+
+            const SizedBox(height: 16),
+
+            //  description
+            Text(
+              "A 4- digit code has been sent to \n+254721 XXX X89",
+              style: Theme.of(context).textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
       ),
     );
   }
