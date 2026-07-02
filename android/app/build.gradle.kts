3ff68e4712d7ca0cd9ec3451d3c21eab43366154
hermajesty842 <hildakinya17@gmail.com>
2026-07-02 17:12:36 +0300

    Modify release build configuration in build.gradle.kts
    Updated release build configuration to use release signing config and enable resource shrinking.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,22 +35,12 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-    // TODO: Add your own signing config for the release build.
-    // Signing with the debug keys for now, so `flutter run --release` works.
-    signingConfig = signingConfigs.getByName("debug")
-    isMinifyEnabled = true
-    proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-    )
-}
-        //release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-           // signingConfig = signingConfigs.getByName("debug")
-       // }
+   buildTypes {
+    getByName("release") {
+        signingConfig = signingConfigs.getByName("release")
+        isMinifyEnabled = true
+        isShrinkResources = true
+        proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
 }
 
